28aa07ee0db9267114730947d86e3bc52b98119d
Anastasiia Sirenko <nastya9701@ukr.net>
2023-02-25 17:41:36 +0200

    corect palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,14 @@
-word = str(input("Please enter a word to check:"))
-def is_it_palindrome(word):
-    if(word == word[::-1]):
-        return("Yes, it is palindrome")
+def palindrome (string):
+    """
+        Prints boolean, based on arguments passed
+        Arguments:
+        word for checking
+    """
+    if(string == string[::-1]):
+        return(True)
     else:
-        return("No, it is not palindrome")
-print(is_it_palindrome(word))
+        return(False)
+print(palindrome("дід"))
+
+
+
